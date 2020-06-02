5bc8ccd1caad9b9a18b509e38cd68ed4ba789abe
umeneri <koishimst@gmail.com>
2020-06-02 09:57:07 +0900

    add chapter 2 examples

---
+++ chapter2/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	GzipWrite()
+}
+
+func OsWrite() {
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	file.Write([]byte("os.File example\n"))
+
+	os.Stdout.Write([]byte("os.Stdout example\n"))
+	os.Stderr.Write([]byte("os.Stdout example error\n"))
+	file.Close()
+}
+
+func ByteBuffer() {
+	var buffer bytes.Buffer
+
+	// buffer.Write([]byte("bytes.Buffer  example\n"))
+	buffer.WriteString("bytes.Buffer  example\n")
+	fmt.Println(buffer.String())
+	fmt.Println(buffer.Len())
+}
+
+func BuilderWrite() {
+	var builder strings.Builder
+	builder.Write([]byte("strings.Builder example\n"))
+	fmt.Println(builder.String())
+}
+
+func NetWrite() {
+	conn, err := net.Dial("tcp", "ascii.jp:80")
+	if err != nil {
+		panic(err)
+	}
+	io.WriteString(conn, "GET /HTTP/1.0\r\nHOST: ascii.jp\r\n\r\n")
+	io.Copy(os.Stdout, conn)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "http.ResponseWriter sample")
+	// http.ResponseWriter inherit io.Writer
+}
+
+func HttpRequest() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func MultiWrite() {
+	var filename = "multiwriter.txt"
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	writer := io.MultiWriter(file, os.Stdout)
+	io.WriteString(writer, "io.MultiWriter example\n")
+	os.Remove(filename)
+}
+
+func GzipWrite() {
+	var filename = "test.txt.gz"
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	// writer is compreessed
+	writer := gzip.NewWriter(file)
+	writer.Header.Name = "test.txt"
+	io.WriteString(writer, "gzip.Writer example\n")
+	writer.Close()
+
+	os.Remove(filename)
+}
