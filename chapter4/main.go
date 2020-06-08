50e3d695ccba31ce18d751b696c8b9f360cb5d30
umeneri <koishimst@gmail.com>
2020-06-09 08:25:33 +0900

    add chapter 4

--- chapter4/main.go
+++ chapter4/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main()  {
+import (
+	"fmt"
+	"time"
+)
 
+func timer(duration int) {
+	fmt.Println("start")
+
+	// done := make(chan string, 1)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+
+		// done <- "result ok"
+	}()
+
+	<-time.After(time.Duration(duration) * time.Second)
+	fmt.Println("timeout")
+}
+
+func main() {
+	timer(2)
 }
